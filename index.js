76864f91ec0039fc0bb411f31f2e57e3f255fa63
Jean Meira <jean.meira10@hotmail.com>
2022-08-02 19:42:42 -0300

    feat: add query params and route params

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const server = express();
 
+// query params = ?nome=NodeJS
+// Route params = /curso/2
+// Request params = {nome: 'NodeJS', tipo: 'Backend'}
+
 //localhost:3000/curso
-server.get('/curso', (req, res) => {
-  return res.json({ msg: 'Hello world!!!' });
+server.get('/curso/:id', (req, res) => {
+  const nome = req.query.nome;
+  const id = req.params.id;
+
+  return res.json({ msg: `Hello ${nome} - curso ${id}` });
 });
 
 server.listen(3333);
